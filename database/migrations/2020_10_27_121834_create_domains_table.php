04d8c7f2da86af3e4842b3cb79323925661c2102
Sarveswara Rao Kanduri <eshwarkanduri@gmail.com>
2020-10-27 17:56:21 +0530

    Auth'

---
+++ database/migrations/2020_10_27_121834_create_domains_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDomainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('domains', function (Blueprint $table) {
+            $table->foreignId('profile_id');
+            $table->string('domain');
+            $table->string('expertise');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('domains');
+    }
+}
